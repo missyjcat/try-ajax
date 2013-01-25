6be0ef54f459054f495238fed8cce3553b210de7
missyjcat <missyjcat@gmail.com>
2013-01-24 19:42:58 -0800

    formatted comments

--- ajax.js
+++ ajax.js
@@ -1,7 +1,11 @@
-// This Ajax example will return the current time (taken from the example PHP script get_time.php) and output it in the second field of index.html.
+// This Ajax example will return the current time (taken from the example PHP script get_time.php) and output it in the 
+// second field of index.html.
+// 
 // It first tests for browser support, then stores a function in the method that processes the response from the server.
-// Then it will make a request to the server to get the data from get_time.php which currently just echos the server's current time.
-// If you were to edit get_time.php and just write "hihihi" index.html's second input field would display "hihihi"
+// 
+// Then it will make a request to the server to get the data from get_time.php which currently just echos the server's 
+// current time. If you were to edit get_time.php and just write "hihihi" index.html's second input field would display 
+// "hihihi"
 
 // Function that creates Ajax object (a new XMLHttpRequest() instance) and tests for browser support
 
@@ -30,7 +34,8 @@ function ajaxFunction(){
 	// Create function that processes response from server. onreadystatechange 
 	ajaxRequest.onreadystatechange = function() {
 
-		// When readyState is 4, that means a response is complete. So we're going to check to see if readyState is 4 and when it is, we will do things:
+		// When readyState is 4, that means a response is complete. So we're going to check to see if readyState is 4 and 
+		// when it is, we will do things:
 		if(ajaxRequest.readyState == 4) {
 
 			// These are the things we will do:
